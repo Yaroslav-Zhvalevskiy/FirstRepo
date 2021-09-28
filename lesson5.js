1e6d2e40e84ae304e94ccbcf87a9d4f2d454c98a
Yaroslav Zhvalevskiy <yaroslav.zhvalevskiy@gmail.com>
2021-09-28 21:35:14 +0300

    classwork

---
+++ lesson5.js
@@ -0,0 +1,24 @@
+let str = "abracadabraamtrbacd";
+let result = "";
+
+for (let i = 0; i < str.length; i++) {
+    if (str[i] == "a") {
+        result += "b";
+    } else if (str[i] == "b") {
+        result += "c";
+    } else if (str[i] == "c") {
+        result += "a";
+    } else {
+        result += str[i];
+    }
+}
+
+console.log(str);
+console.log(result);
+
+
+
+
+
+
+
